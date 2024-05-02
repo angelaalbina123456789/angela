4ef627e3519141114c160986de72ddd358ac1cbe
anelaalbina12345678 <shermatalieva06@gmail.com>
2024-05-02 16:05:54 +0700

    Добавили функцию чтения из файла wrfile.txt

--- wrfile.c
+++ wrfile.c
@@ -2,20 +2,41 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
-int main(int argc, char *argv[], char *envp[]) {
-	printf("Количество параметров, переданных программе: %d", argc);
-	if (argc>1) {
-		for(int  i=0; i<argc; i++) {
-			printf("Параметр %d: %s\n", i, argv[i]);
-		}
-	}
-	else {
-		int j=0; 
-			while (envp[j]!=NULL) {
-			printf("Параметр окружения %d: %s\n", j, envp[j]);
-			j++;
-		}
-	}
-	return 0;
+#include <fcntl.h>
+
+int main() {
+        int fd;
+        size_t size;
+        char string[] = "Gr. 3.008.2.23";
+        if((fd = open("wrfile.txt", O_WRONLY|O_CREAT, 0666))< 0) {
+                printf("Ошибка при открытии файла на запись\n");
+                exit(-1);
+        }
+        size = write(fd, string, 14);
+        if(size !=14) {
+                printf("Не удалось записать 14 байт в файл\n");
+                exit(-1);
+        }
+        else printf("Строка записана в файл\n");
+        if (close(fd) < 0) {
+                printf("Не получилось закрыть файл\n");
+                exit(-1);
+        }
+        char resstring[14];
+        if((fd = open("wrfile.txt", O_RDONLY)) < 0) {
+                printf("Ошибка при открытии файла на чтение\n");
+                exit(-1);
+        }
+        size = read(fd, resstring, 14);
+        if (size != 14) {
+                printf("Не удалось прочитать 14 байт из файла\n");
+                exit(-1);
+        }
+        else printf("Прочитанная строка: %s\n", resstring);
+        if (close(fd) < 0) {
+                printf("Не получилось закрыть файл\n");
+                exit(-1);
+        }
+        return 0;
 }
 
